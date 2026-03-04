020edd03760710bba39da1e13bd0ce613ede62b1
GedMarc <ged_marc@hotmail.com>
2026-03-04 05:27:11 +0200

    Update Test Library structure and module declarations
    Revised module descriptors and renamed `com.jwebmp.testlib` to `com.jwebmp.testing`. Added exports for new packages (`com.jwebmp.testing` and `com.jwebmp.testing.services`), specified `uses` declarations, and introduced a new test module `com.jwebmp.testing.test`. Updated README build badge reference.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,21 @@
-module com.jwebmp.testlib {
-    requires  transitive  com.guicedee.guicedinjection;
+open module com.jwebmp.testing {
+
+
+    exports com.jwebmp.testing;
+    exports com.jwebmp.testing.services;
+
+    requires transitive com.guicedee.vertx.web;
+
     requires java.net.http;
     requires jdk.httpserver;
     requires static lombok;
+
+    requires java.logging;
+
     requires transitive org.junit.jupiter.api;
+    requires com.guicedee.guicedinjection;
+
+    uses com.jwebmp.testing.services.ITestInstanceDestroyService;
+    uses com.jwebmp.testing.services.ITestInstanceInitializerService;
+    uses com.jwebmp.testing.services.ITestInstanceResetService;
 }
---
+++ src/test/java/module-info.java
@@ -0,0 +1,6 @@
+module com.jwebmp.testing.test {
+
+    requires transitive com.jwebmp.testing;
+    requires org.junit.jupiter.api;
+
+}
